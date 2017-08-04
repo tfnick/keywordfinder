01d5928e7c467b3f5ca30fa41156138274cbd464
wenxue.liu <wenxue.liu@cafintech.com>
2017-08-04 10:20:20 +0800

    for test shell

---
+++ shell/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[  -e `dirname $0`/env.sh ]  && . `dirname $0`/env.sh
+
+echo $APP_NAME
---
+++ shell/env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+APP='keywordfinder'
+APP_CHECKOUT_HOME='C:/Users/nick/PycharmProjects/keywordfinder/'
