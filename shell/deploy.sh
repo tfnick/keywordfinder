11cf1a7383365054cd48d3ac2682bff2e94a2f1a
vmware <2595925500@qq.com>
2017-08-04 16:52:54 +0800

    add file read_until_right

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
-[  -e `dirname $0`/env.sh ]  && . `dirname $0`/env.sh
+#test and import env.sh
+[ -e `dirname $0`/env.sh ]  && . `dirname $0`/env.sh
 
-echo $APP_NAME
+#echo ${PROJECT_NAME}
+#echo `date "+%Y-%m-%d"`
+#echo $(date "+%Y-%m-%d")
+#A=(a b c def)
+#echo ${A[3]}
+#echo ${#A[@]}
+#a1=1;a2=2;a3=3
+#echo $((a1+a2+a3))
+#s1="China is an lovely country."
+
+read_until_right "choose which to deploy(0|enter all,1 api,2 web): " "0" "1" "2"
+
+echo "read_until_right output : $? "
+
+
+
+
+
+
+
+
+
+#if [ ${app} == '1' ]
+#then
+#   echo "a 等于 b"
+#elif [ ${app} == '2' ]
+#then
+#   echo "a 大于 b"
+#elif [ ${app} == '0' ]
+#then
+#   echo ""
+#else
+#   echo read_until_right '$@'
+#fi
+
+
--- shell/env.sh
+++ shell/env.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 PROJECT_NAME='keywordfinder'
-APP_CHECKOUT_HOME='C:/Users/nick/PycharmProjects/'
+CHECKOUT_HOME='/home/nick/PycharmProjects/'
+
+#
+# info var1(default) var2 var3
+function read_until_right() {
+
+    read -p "$1" input
+    if test -z ${input}
+    then
+         input="0"
+    fi
+
+    found=0
+    for e in "$@"
+    do
+        #echo "param ${e} "
+        if [ "${e}" = "${input}" ]
+        then
+            found=1
+        fi
+    done
+
+    if [ ${found} -eq 1 ]
+    then
+        return "${input}"
+    else
+        echo "invalid input,please input again."
+        read_until_right "$@"
+    fi
+
+}
